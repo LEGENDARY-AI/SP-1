494b10f5f66e6724384fe941e6f638b968c88a51
Andy Gambo <gamboandy444@gmail.com>
2025-01-13 09:14:32 +0000

    Add files via upload

---
+++ main.py
@@ -0,0 +1,9 @@
+
+
+item=input("what will you like to buy?:")
+price=int(input("what is the price ?:"))
+quantity=int(input("how many would you like to buy?"))
+
+total=price* quantity
+
+print(f"your total price is {total}")
